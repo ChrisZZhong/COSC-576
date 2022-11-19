ef28a3a40e235df495921907ab01424ad5254b00
ChrisZZHong <zz397@georgetown.edu>
2022-11-18 21:59:07 -0500

    update demo

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from torch.optim import optimizer
 from tqdm.notebook import tqdm
 from pathlib import Path
 import torch
@@ -16,8 +17,8 @@ pretrained = False
 train_input_path = "./testing_train"
 test_input_path = "./test"
 valid_input_path = "./valid"
+output_path = "./outputs/vgg19_test.pth"
 outputs = Path('./outputs')
-
 
 if not outputs.is_dir():
     outputs.mkdir()
@@ -29,9 +30,34 @@ print(torch.version.cuda)
 print(torch.cuda.is_available())  # true
 print(device)
 
-##
+
+# save model
+def save_model(epochs, model, optimizer, criterion, output_Path='./outputs/model.pth', descr=''):
+    torch.save({
+        'epoch': epochs,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': criterion,
+        'descr': descr,
+    }, output_Path)
+
+
+# image augment
 
 train_trans = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+    transforms.RandomRotation(degrees=(30, 70)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.5, 0.5, 0.5],
+        std=[0.5, 0.5, 0.5]
+    )
+])
+
+valid_trans = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(
@@ -39,8 +65,6 @@ train_trans = transforms.Compose([
         std=[0.5, 0.5, 0.5]
     )
 ])
-valid_trans = train_trans
-
 # read file
 
 train_folder = ImageFolder(train_input_path, transform=train_trans, )
@@ -51,7 +75,6 @@ test_folder = ImageFolder(test_input_path, transform=valid_trans, )
 
 train_loader = torch.utils.data.DataLoader(train_folder, shuffle=True, batch_size=BATCH_SIZE)
 test_loader = torch.utils.data.DataLoader(test_folder, shuffle=False, batch_size=BATCH_SIZE)
-
 
 ###########################################
 
@@ -83,8 +106,6 @@ def plot_losses_and_acc(train_losses, train_accuracies, valid_losses, valid_accu
     axes[1].plot(valid_accuracies, label='valid_losses')
     axes[1].set_title('Accuracy')
     axes[1].legend()
-
-
 
 
 def validate(model, valid_data, loss_fn):
@@ -136,26 +157,15 @@ def train(model, train_data, valid_data, loss_fn, opt, epoches=5):
     return model, train_losses, train_accuracies, valid_losses, valid_accuracies
 
 
+################################################
+
 vgg, train_losses, train_accuracies, valid_losses, valid_accuracies = train(vgg, train_loader, test_loader, loss_fn,
                                                                             opt, epoches=epoches)
 
-
-# save model
-def save_model(epochs, model, optimizer, criterion, name='model', descr=''):
-    torch.save({
-        'epoch': epochs,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'loss': criterion,
-        'descr': descr,
-    }, f'outputs/{name}.pth')
-
-
-save_model(epoches, vgg, opt, loss_fn, 'vgg19_test', descr='15 unfrozen layers; 1e-4 lr')
+save_model(epoches, vgg, opt, loss_fn, output_path, descr='15 unfrozen layers; 1e-4 lr')
 
 valid_folder = ImageFolder(valid_input_path, transform=valid_trans, )
 valid_loader = torch.utils.data.DataLoader(valid_folder, shuffle=False, batch_size=BATCH_SIZE)
 valid_loss, valid_acc = validate(vgg, valid_loader, loss_fn)
 print(valid_loss, valid_acc)
 plt.show()
-
